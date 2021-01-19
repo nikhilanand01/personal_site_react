ecbe27ac8f1e4dd852ebcb8687c864dc76021fbb
nikhilanand01 <nikhil@fudinc.com>
2021-01-19 18:50:52 +0100

    fixin thangs

--- src/components/Social.js
+++ src/components/Social.js
@@ -9,7 +9,7 @@ class Social extends Component {
               <a href="http://nikhilanand3.medium.com" target="_blank" rel="noopener noreferrer"><i class="fab fa-medium"></i></a>
               <a href="https://github.com/nikhilanand01" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
               <a href="https://soundcloud.com/nikhilanand3" target="_blank" rel="noopener noreferrer"><i class="fab fa-soundcloud"></i></a>
-              <a href="https://Google.com" target="_blank" rel="noopener noreferrer"><i class="fas fa-file"></i></a>
+              <a href="https://raw.githubusercontent.com/nikhilanand01/personal_site_react/9183e11c661b167a08276c7809fa8057b7ee9c62/src/images/NAnand_Resume_0121.pdf" target="_blank" rel="noopener noreferrer"><i class="fas fa-file"></i></a>
             </div>
             )
         }

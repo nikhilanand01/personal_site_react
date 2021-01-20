e5c90f87b751875bf8fded16c7691cf53987b0a2
nikhilanand01 <nikhil@fudinc.com>
2021-01-20 21:40:03 +0100

    mobile version hidden for now

--- src/App.js
+++ src/App.js
@@ -9,34 +9,41 @@ import Skills from './pages/Skills.js';
 import Contact from './pages/Contact.js';
 import Experience from './pages/Experience.js';
 import Projects from './pages/Projects.js';
-
+import {BrowserView, MobileOnlyView} from 'react-device-detect';
 
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/education">
-          <Education />
-        </Route>
-        <Route path="/experience">
-          <Experience />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/skills">
-          <Skills />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-      </div>
-    </Router>
+  <div>
+    <BrowserView>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/education">
+            <Education />
+          </Route>
+          <Route path="/experience">
+            <Experience />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/skills">
+            <Skills />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </div>
+      </Router>
+    </BrowserView>
+    <MobileOnlyView>
+      <h2>Mobile Version Coming Soon. For now you can see my site on a desktop!</h2>
+    </MobileOnlyView>
+  </div>
     );
   }
 

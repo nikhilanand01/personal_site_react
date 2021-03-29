e921eefe91dfa8cc2472be680addb15072687e25
nikhilanand01 <nikhil@fudinc.com>
2021-03-29 20:37:49 +0200

    fixing project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -15,16 +15,16 @@ class ProjectCard extends Component {
                 </div>
                 <div class="card__content">
                   <h2 class="card__heading">
-                    <a class="card__link" href={this.props.link} target="_blank">{this.props.projectName}</a>
+                    <a class="card__link" href={this.props.link} target="_blank" rel="noreferrer">{this.props.projectName}</a>
                   </h2>
-                  <a href={this.props.link} target="_blank" class="card__category" style={{padding: '10px'}}>{this.props.category}</a>
+                  <a href={this.props.link} target="_blank" rel="noreferrer" class="card__category" style={{padding: '10px'}}>{this.props.category}</a>
                   <div class="card__description">
                     <p style={{fontSize: '15px'}}>{this.props.description}</p>
                   </div>
                 </div>
                 <ul class="card__actions">
                   <li class="card__action">
-                    <a href={this.props.link} target="_blank" class="button">See More</a>
+                    <a href={this.props.link} target="_blank" rel="noreferrer" class="button">See More</a>
                   </li>
                 </ul>
               </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,10 +15,10 @@ class Projects extends Component {
               <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly'}}>
                 <ProjectCard image={whatsmystream} label="Website" category="ReactJS" link="https://whatsmystream.com" projectName="What's My Stream" description="A Music Streaming Calculator"/>
                 <ProjectCard image={fiveDot} label="Website" category="HTML/CSS" link="https://5degreestravel.com" projectName="5 Degrees Travel" description="Travel Blog / Reccomendations"/>
-                <ProjectCard image={billi} label="Data Analysis" category="R"  projectName="Top 10?" description="Predicting Billboard Hot 100 Top 10 Songs"/>
+                <ProjectCard image={billi} label="Data Analysis" category="R" link="#" projectName="Top 10?" description="Predicting Billboard Hot 100 Top 10 Songs"/>
                 <ProjectCard image={hacks} label="Mobile App" category="React Native" link="https://github.com/nikhilanand01/outsidehacks" projectName="Outside Hacks" description="Scavenger Hunt for Outside Lands Hackathon"/>
-                <ProjectCard image={fud} label="Mobile App" category="Swift"  projectName="fud" description="App to lower food waste with discounts"/>
-                <ProjectCard image={lboard} label="Mobile App" category="React Native"  projectName="LBoard" description="App to share embarssing moments with friends"/>
+                <ProjectCard image={fud} label="Mobile App" category="Swift"  link="#" projectName="fud" description="App to lower food waste with discounts"/>
+                <ProjectCard image={lboard} label="Mobile App" category="React Native" link="#" projectName="LBoard" description="App to share embarssing moments with friends"/>
               </div>
             </div>
             )
